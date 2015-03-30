8320da2b71e1324bdaa5a66780ad141dd49706b5
csjlengxiang <csjlengxiang@gmail.com>
2015-03-30 22:27:08 +0800

    mess
    get

--- Simulator/BLL/LogService.cs
+++ Simulator/BLL/LogService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,20 @@ namespace BLL
 {
     public class LogService
     {
-        public static Mess(string str)
+        public static void Mess(string data, string path = @"c:\GpsMessges")
         {
+            try
+            {
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+                StreamWriter sw = File.AppendText(path + @"\" + DateTime.Now.ToString("yyyyMMdd") + ".txt");
+                sw.WriteLine("【" + DateTime.Now.ToString() + "】" + data);
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
 
+            }
         }
     }
 }
